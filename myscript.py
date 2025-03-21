90d18c3fd5a4142d1c2e86e7e9b649b4bfd810de
YanisOuld <yanis.ould-mahammed@polymtl.ca>
2025-03-21 10:59:29 -0400

    Pt la derniere version

--- myscript.py
+++ myscript.py
@@ -9,7 +9,7 @@ os.system(f"git bisect start {bad_hash} {good_hash}")
 
 # Utilisation de git bisect run pour exécuter les tests sur chaque commit
 # Remplacer "pytest" par la commande qui exécute les tests dans ton projet
-os.system("git bisect run py manage.py test")
+os.system("git bisect run python manage.py test")
 
 # Réinitialisation de git bisect à la fin du processus
 os.system("git bisect reset")
